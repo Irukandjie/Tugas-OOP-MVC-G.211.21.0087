1c4f8d4220227f30445e02d6228fcca16f165504
aldenchristtw@gmail.com <aldenchristtw@gmail.com>
2022-10-04 21:54:29 +0700

    Kelazzz

---
+++ Lat4_2b.php
@@ -0,0 +1,4 @@
+<?php
+require_once("lat4_2a.php");
+$mhs2 = new mahasiswa("Alden Christian", "G.211.21.0087", "FTIK", "Semester 3");
+$mhs2->cetak();
---
+++ Lat4_3b.php
@@ -0,0 +1,7 @@
+<?php
+require_once("lat4_3a.php");
+$mhs1 = new mahasiswa();
+$mhs1->nama = "Alden Christian Totonafo Waruwu </br></br>";
+$mhs1->nim = "G.211.21.0087";
+echo $mhs1->nama;
+echo $mhs1->nim;
